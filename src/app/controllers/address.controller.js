63ce03652ff314bc3a404cb4bbed91c4b1478b2a
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-06-09 17:32:00 +0700

    Fix session transaction

--- src/app/controllers/address.controller.js
+++ src/app/controllers/address.controller.js
@@ -5,18 +5,21 @@ const APIFeatures = require('../../utils/APIFeatures.js');
 const getCities = asyncHandler(async (req, res) => {
     const features = new APIFeatures(City, req.query).filter().sort().limitFields();
     const cities = await features.query;
+    req.session.endSession();
     res.status(200).json(cities);
 });
 
 const getDistricts = asyncHandler(async (req, res) => {
     const features = new APIFeatures(District, req.query).filter().sort().limitFields();
     const districts = await features.query;
+    req.session.endSession();
     res.status(200).json(districts);
 });
 
 const getWards = asyncHandler(async (req, res) => {
     const features = new APIFeatures(Ward, req.query).filter().sort().limitFields();
     const wards = await features.query;
+    req.session.endSession();
     res.status(200).json(wards);
 });
 
