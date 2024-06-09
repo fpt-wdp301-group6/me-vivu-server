22583905155abfea69a8d3e1921437e571b73eaa
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-06-09 23:32:45 +0700

    Room and Seat API

---
+++ src/app/controllers/room.controller.js
@@ -0,0 +1,150 @@
+const asyncHandler = require('express-async-handler');
+const Theater = require('../models/Theater');
+const Room = require('../models/Room');
+const Seat = require('../models/Seat');
+const { ErrorWithStatus } = require('../../utils/error');
+
+const createRoom = asyncHandler(async (req, res) => {
+    const session = req.session;
+    const { name } = req.body;
+    const theater = await Theater.findOne({ _id: req.params.theaterId, cinema: req.user.cinema })
+        .select('rooms')
+        .session(session);
+
+    if (!theater) {
+        throw new ErrorWithStatus('Rạp chiếu không tồn tại', 404);
+    }
+
+    if (theater.rooms.some((room) => room.name === name)) {
+        throw new ErrorWithStatus('Tên phòng đã tồn tại', 409);
+    }
+
+    const room = new Room(req.body);
+    theater.rooms.push(room);
+    await Promise.all([theater.save({ session }), room.save({ session })]);
+
+    await session.commitTransaction();
+    session.endSession();
+    res.json({ data: room, message: 'Phòng chiếu được tạo thành công' });
+});
+
+const updateRoom = asyncHandler(async (req, res) => {
+    const session = req.session;
+    const room = await Room.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true, session });
+    if (!room) {
+        throw new ErrorWithStatus('Phòng chiếu không tồn tại', 404);
+    }
+
+    await session.commitTransaction();
+    session.endSession();
+    res.status(201).json({ data: room, message: 'Phòng chiếu được cập nhật thành công' });
+});
+
+const deleteRoom = asyncHandler(async (req, res) => {
+    const session = req.session;
+    const roomId = req.params.id;
+    const food = await Room.findById(roomId).session(session);
+    if (!food) {
+        throw new ErrorWithStatus('Phóng chiếu không tồn tại', 404);
+    }
+
+    await Promise.all([
+        food.deleteOne({ session }),
+        Theater.updateMany({ rooms: roomId }, { $pull: { rooms: roomId } }, { session }),
+    ]);
+
+    await session.commitTransaction();
+    session.endSession();
+    res.status(201).json({ data: food, message: 'Phóng chiếu được xóa thành công' });
+});
+
+const getRooms = asyncHandler(async (req, res) => {
+    const session = req.session;
+    const theater = await Theater.findOne({ _id: req.params.theaterId, cinema: req.user.cinema })
+        .select('rooms')
+        .populate('rooms');
+
+    if (!theater) {
+        throw new ErrorWithStatus('Rạp chiếu không tồn tại', 404);
+    }
+
+    session.endSession();
+    res.status(201).json({ data: theater.rooms });
+});
+
+const getSeats = asyncHandler(async (req, res) => {
+    const session = req.session;
+    const room = await Room.findById(req.params.id).select('seats').populate('seats');
+    if (!room) {
+        throw new ErrorWithStatus('Phòng chiếu không tồn tại', 404);
+    }
+
+    session.endSession();
+    res.status(201).json({ data: room.seats });
+});
+
+const changeSeatMap = asyncHandler(async (req, res) => {
+    const session = req.session;
+    const room = await Room.findById(req.params.id).select('seats').session(session);
+    if (!room) {
+        throw new ErrorWithStatus('Phòng chiếu không tồn tại', 404);
+    }
+
+    const seatOperations = req.body.seats;
+    const bulkOperations = [];
+    seatOperations.forEach((seat, index) => {
+        const isDuplicatedName = seatOperations.slice(index + 1).some((item) => item.name === seat.name);
+        if (isDuplicatedName) {
+            throw new ErrorWithStatus('Số ghế trùng nhau', 409);
+        }
+        const isDuplicatedCoordinates = seatOperations
+            .slice(index + 1)
+            .some((item) => item.x === seat.x && item.y === seat.y);
+        if (isDuplicatedCoordinates) {
+            throw new ErrorWithStatus('Vị trí ghế trùng nhau', 409);
+        }
+
+        if (seat._id) {
+            if (seat.name === '') {
+                bulkOperations.push({
+                    deleteOne: {
+                        filter: { _id: seat._id },
+                    },
+                });
+                room.seats = room.seats.filter((seatId) => !seatId.equals(seat._id));
+            } else {
+                bulkOperations.push({
+                    updateOne: {
+                        filter: { _id: seat._id },
+                        update: { $set: seat },
+                    },
+                });
+            }
+        } else {
+            bulkOperations.push({
+                insertOne: {
+                    document: seat,
+                },
+            });
+        }
+    });
+
+    const result = await Seat.bulkWrite(bulkOperations, { session });
+
+    const newSeats = result.insertedIds ? Object.values(result.insertedIds) : [];
+    room.seats = room.seats.concat(newSeats);
+    await room.save();
+
+    await session.commitTransaction();
+    session.endSession();
+    res.json({ message: 'Bản đồ ghế được cập nhật thành công' });
+});
+
+module.exports = {
+    createRoom,
+    updateRoom,
+    deleteRoom,
+    getRooms,
+    getSeats,
+    changeSeatMap,
+};
--- src/app/models/Seat.js
+++ src/app/models/Seat.js
@@ -6,16 +6,35 @@ const SeatTypeEnum = {
     Couple: 'couple',
 };
 
+const SeatStatusEnum = {
+    Available: 'available',
+    Reserved: 'reserved',
+    Broke: 'broke',
+};
+
 const Seat = new Schema(
     {
         name: {
             type: String,
             required: true,
         },
+        x: {
+            type: Number,
+            required: true,
+        },
+        y: {
+            type: Number,
+            required: true,
+        },
         type: {
             type: String,
-            enum: [SeatTypeEnum.Normal, SeatTypeEnum.VIP, SeatTypeEnum.Couple],
+            enum: Object.values(SeatTypeEnum),
             default: SeatTypeEnum.Normal,
+        },
+        status: {
+            type: String,
+            enum: Object.values(SeatStatusEnum),
+            default: SeatStatusEnum.Available,
         },
     },
     { timestamps: true },
