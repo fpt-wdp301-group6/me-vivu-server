311a5f3b6871896ee47c24e1be26e6fb29ff2eeb
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-07-21 22:53:19 +0700

    Get my ticket

--- src/app/routes/ticket.route.js
+++ src/app/routes/ticket.route.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { buyTicket, createPaymentLink, receiveWebhook } = require('../controllers/ticket.controller');
+const { buyTicket, createPaymentLink, receiveWebhook, getTickets } = require('../controllers/ticket.controller');
+const { isUser } = require('../middlewares/authenticate');
 
 const router = Router();
 
 router.post('/', buyTicket);
 router.post('/:id/payment-link', createPaymentLink);
 router.post('/receive-hook', receiveWebhook);
+router.get('/me', isUser, getTickets);
 
 module.exports = router;
